d17111a5ca2ec8f90fb0b3863a85a02c0b13af46
Sarabjeet Singh <sarabjeet@thoughtscrap.com>
2018-10-26 15:48:37 +0530

    Cleaned up the script by moving common functionality into functions. Added user interaction code to determine the working directory. Added error checking code for simple scenarios.

--- make_kernel.sh
+++ make_kernel.sh
@@ -1,22 +1,33 @@
 #! /bin/bash
 
 ########################################
-## Constants
+## Commands
 ########################################
 _ECHO=/bin/echo
+_GREP=/bin/grep
+_TAR=/bin/tar
 _DPKG=/usr/bin/dpkg-query
 _LSCPU=/usr/bin/lscpu
+_WGET=/usr/bin/wget
+_INSTALL_PKG="sudo /usr/bin/apt-get -y install"
+_MKDIR_P="/bin/mkdir -p"
 
+_READ="read -p"
+
+########################################
+## Constants
+########################################
 REQ_PKGS="bison flex build-essential libssl-dev device-tree-compiler python python-dev swig"
 TOOLCHAIN_URL="https://releases.linaro.org/components/toolchain/binaries/7.3-2018.05/arm-linux-gnueabihf"
-WORK_DIR_REL=workdir
+TOOLCHAIN_DIR=toolchain
+
 
 ########################################
 ## Functions
 ########################################
 check_pkg_installed(){
     local pkg_name=${1}
-    local status=$(${_DPKG} -W -f'${Status}' ${pkg_name} 2>/dev/null | grep -c "ok installed")
+    local status=$(${_DPKG} -W -f'${Status}' ${pkg_name} 2>/dev/null | ${_GREP} -c "ok installed")
     if [ ${status} -ne 0 ]
     then
         return 0
@@ -31,7 +42,7 @@ ensure_pkg_installed(){
     if [ $? -eq 1 ]
     then
         ${_ECHO} "${pkg_name} is not installed, installing..."
-        sudo apt-get install -y ${pkg_name}
+        ${_INSTALL_PKG} ${pkg_name}
         return $?
     else
         ${_ECHO} "${pkg_name} is already installed..."
@@ -40,7 +51,7 @@ ensure_pkg_installed(){
 }
 
 get_architecture(){
-    local arch=$(${_LSCPU} | grep "Architecture" | grep -c "x86_64")
+    local arch=$(${_LSCPU} | ${_GREP} "Architecture" | ${_GREP} -c "x86_64")
     if [ ${arch} -eq 0 ]
     then
         return 32
@@ -50,6 +61,14 @@ get_architecture(){
 }
 
 install_toolchain(){
+    local OUT_DIR=${1}
+    ${_MKDIR_P} ${OUT_DIR}
+    if [ ! -d ${OUT_DIR} ]
+    then
+        ${_ECHO} " Unable to create toolchain directory ${OUT_DIR}, cannot continue..."
+	exit 1
+    fi
+
     ## Check the machine architecture
     get_architecture
     local arch=$?
@@ -58,44 +77,90 @@ install_toolchain(){
     then
 	## 32 bit machine ##
 	${_ECHO} "Installing 32 bit toolchain..."
-	wget ${TOOLCHAIN_URL}/gcc-linaro-7.3.1-2018.05-i686_arm-linux-gnueabihf.tar.xz -O toolchain.tar.xz
-        tar -xf toolchain.tar.xz
+	${_WGET} ${TOOLCHAIN_URL}/gcc-linaro-7.3.1-2018.05-i686_arm-linux-gnueabihf.tar.xz -O /tmp/toolchain.tar.xz
+        ${_TAR} -xf /tmp/toolchain.tar.xz -C ${OUT_DIR} --strip-components 1
     elif [ ${arch} -eq 64 ]
     then
 	## 64 bit machine ##
 	${_ECHO} "Installing 64 bit toolchain..."
-	wget ${TOOLCHAIN_URL}/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf.tar.xz -O toolchain.tar.xz
-        tar -xf toolchain.tar.xz
+	${_WGET} ${TOOLCHAIN_URL}/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf.tar.xz -O /tmp/toolchain.tar.xz
+        ${_TAR} -xf /tmp/toolchain.tar.xz -C ${OUT_DIR} --strip-components 1
     else
 	## Unknown architecture ##
 	${_ECHO} "Unknown Archtecture..."
     fi
 }
 
+display_information(){
+    ${_ECHO} "This script will perform the following actions:"
+    ${_ECHO} ""
+    ${_ECHO} "1. Check for the necessary dependencies and install the missing ones."
+    ${_ECHO} "2. Install the cross compilation toolchain."
+    ${_ECHO} "3. Clone the linux kernel and WiFi driver source repositories."
+    ${_ECHO} "4. Compile the linux kernel and WiFi driver."
+    ${_ECHO} ""
+    ${_READ} "Do you wish to continue? Y/n (n):" option
+
+    if [ -z ${option} ] || [ ${option} != "Y" ]
+    then
+        ${_ECHO} "Exiting..."
+        exit 0
+    fi
+}
+
+
+install_dependencies() {
+    sudo apt-get update
+
+    for dependency in ${REQ_PKGS}
+    do
+        ensure_pkg_installed ${dependency}
+    done
+}
+
+
+create_base_dir(){
+local BASE
+while [ -z ${BASE} ]
+do
+    ${_READ} "Specify which directory to use as the base for work (Should be non-existent):" BASE
+    if [ -d ${BASE} ]
+    then
+        ${_ECHO} "${BASE} exists, Please enter a non-existant directory path"
+	unset BASE
+	continue
+    else
+        ${_MKDIR_P} ${BASE}
+	if [ -d ${BASE} ]
+	then
+            export BASE_DIR=${BASE}
+            break
+	else
+            ${_ECHO} "Unable to create ${BASE}, cannot continue..."
+	    exit 1
+        fi
+    fi
+done
+}
 
 ########################################
 ## Entry Point
 ########################################
 
-echo "Creating a new Working Directory \"${WORK_DIR_REL}\""
-mkdir ${WORK_DIR_REL}
-cd ${WORK_DIR_REL}
+display_information
 
+create_base_dir
 
 ## Ensure all the required packages are installed ##
-sudo apt-get update
-
-for req_pkg in ${REQ_PKGS}
-do
-    ensure_pkg_installed ${req_pkg}
-done
-
+install_dependencies
 
 ## Install the toolchain for cross compilation
-install_toolchain
+install_toolchain ${BASE_DIR}/${TOOLCHAIN_DIR}
+export CROSS_COMPILE_FLAG=${TOOLCHAIN_DIR}/bin/arm-linux-gnueabihf-
 
 
 ## Clone the kernel source repository and xradio driver source repository
+cd ${BASE_DIR}
 
 ${_ECHO} "Downloading kernel source tree..."
 git clone --depth 1 -b sunxi-next --single-branch https://github.com/linux-sunxi/linux-sunxi
@@ -106,4 +171,5 @@ git clone --depth 1 https://github.com/fifteenhex/xradio
 ${_ECHO} "Create .config file for kernel"
 cp ../.config linux-sunxi/
 
-${_ECHO} "Compiling kernel..."
+${_ECHO} "Compiling kernel (This may take a long time)..."
+
